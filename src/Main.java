9bed21320d7f33cdb221f0c439eb78c24b90cbe7
shaggy1703 <pala4-88@yandex.ru>
2025-04-25 10:05:56 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task 1");
+
+        int needSave = 2459000;
+        int startAmount = 0;
+        int amountDefferal = 15000;
+        int month = 0;
+
+        while (startAmount<needSave){
+            month ++;
+            startAmount += amountDefferal;
+        }
+        System.out.println("Месяц " + month + ", сумма накоплений равна " + startAmount + " рублей.");
+
+        System.out.println();
+
+        System.out.println("Task 2");
+
+        int i = 1;
+        while (i <= 10){
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        for (int j = 10; j >= 1; j--){
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println("Task 3");
+
+
+
+
+
+
     }
 }
